36248edcc4eee725545e8227f6099d14eff8b20c
josel82 <jose.padilla.dev@gmail.com>
2018-02-22 14:10:57 +0000

    Code clean-up in the calculator.service

--- src/app/services/calculator.service.ts
+++ src/app/services/calculator.service.ts
@@ -5,14 +5,33 @@ import { UnitConverterService } from './unit-converter.service';
 @Injectable()
 export class CalculatorService {
 
+  //Protein/Fat factors Estimation ============================================
+  private protMult:number = 1.8; // g of protein per Kg of weight. (Recommended)
+  private fatMult:number = .25; // % fat contribuition in the TDEE
+  // ==========================================================================
+
+  //Macro to Calorie convertion rates==========================================
+  private protToCal:number = 4; // calories per gram of protein
+  private carbsToCal:number = 4; // calories per gram of Carbohydrates
+  private fatToCal:number = 9; // calories per gram of Fat
+  //===========================================================================
+
+  //Mifflin-St Jeor’s Formula coefficients and constants=======================
+  private bmrWeightMult:number = 10;
+  private bmrHeightMult:number = 6.25;
+  private bmrAgeMult:number = 5;
+  private bmrMaleConst:number = 5;
+  private bmrFemaleConst:number = 161;
+  //===========================================================================
+
   constructor(private converter: UnitConverterService) { }
 
   calcBMR(gender, weight, height, age){
 
     if(gender === 0){
-      return Math.floor((10 * weight) + (6.25 * height) - (5 * age) + 5);
+      return Math.floor((this.bmrWeightMult * weight) + (this.bmrHeightMult * height) - (this.bmrAgeMult * age) + this.bmrMaleConst);
     }else if(gender === 1){
-      return Math.floor((10 * weight) + (6.25 * height) - (5 * age) - 161);
+      return Math.floor((this.bmrWeightMult * weight) + (this.bmrHeightMult * height) - (this.bmrAgeMult * age) - this.bmrFemaleConst);
     }
   }
 
@@ -24,16 +43,16 @@ export class CalculatorService {
     return Math.floor(tdee * goal);
   }
 
-  calcProtein(weight){  
-      return Math.floor(weight * 1.8);
+  calcProtein(weight){
+      return Math.floor(weight * this.protMult);
   }
 
   calcCarb(totalCal, prot, fat){
-    return Math.floor((totalCal - (prot * 4 + fat * 9))/4);
+    return Math.floor((totalCal - (prot * this.protToCal + fat * this.fatToCal))/this.carbsToCal);
   }
 
   calcFat(totalCal){
-    return Math.floor((totalCal * 0.25)/9);
+    return Math.floor((totalCal * this.fatMult)/this.fatToCal);
   }
 
 }
